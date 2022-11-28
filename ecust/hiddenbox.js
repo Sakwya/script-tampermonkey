1b80d74fb5052b1b2222291f655302302b7cd68b
Skuyazlu <celiasash@foxmail.com>
2022-11-28 19:08:20 +0800

    fix

--- ecust/hiddenbox.js
+++ ecust/hiddenbox.js
@@ -11,32 +11,29 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
-    //辅助方法>>打印debug信息
-    const p = str => console.log(str);
-    //p("预定义输出开始");
-    const creatdiv = id => {
-        var div = document.createElement('div');
-        div.id = id;
-        return div;
-    }
-    //去除大概不需要的信息
-    var course_list = document.body.getElementsByClassName("wzy_couros_row");
-    var course_r = creatdiv("wzy_couros");
-    course_r.className = ("wzy_couros");
-    for (let i = 0; i < course_list.length; i++) {
-        course_list[i].setAttribute("cid", course_list[i].parentElement.getAttribute("cid"));
-        course_r.appendChild(course_list[i].cloneNode(true));
-    }
-    document.body.innerHTML = "";
-    document.body.appendChild(course_r);
-    //定义样式
-    const style = `
+  'use strict';
+  //辅助方法>>打印debug信息
+  const p = str => console.log(str);
+  //p("预定义输出开始");
+  const creatdiv = id => {
+    var div = document.createElement('div');
+    div.id = id;
+    return div;
+  }
+  //去除大概不需要的信息
+  var course_r = document.body.getElementsByClassName("wzy_couros")[0];
+  document.body.appendChild(course_r);
+  var pog = document.body.childNodes;
+  while (pog.length > 1) {
+    document.body.removeChild(pog[0]);
+  }
+  //定义样式
+  const style = `
     body {
         background: #0000;
       }
       .wzy_couros{
-        width: 800px;
+        width: 60%;
         margin: auto;
     }
       .wzy_couros_row {
@@ -129,62 +126,63 @@
         line-height: 20px;
       }
     `
-    GM_addStyle(style);
-    //定义下拉栏
+  GM_addStyle(style);
+  //定义下拉栏
 
-    var course_ele = course_r.children;
-    var course_root = creatdiv("course_root");
-    var course_b_row = creatdiv("course_b_row");
-    for (let i = 0; i < course_ele.length; i++) {
-        let course_b = document.createElement("div");
-        course_b.className = ("course_b");
-        course_b.id = course_ele[i].getAttribute("cid");
-        course_b.addEventListener("click", function () {
-            let course_r = document.getElementById(this.id);
-            if (this.className == "course_b") {
-                this.className = "course_b_h"
-                document.querySelector("[cid='" + this.id + "']").style.display = 'none';
-                let x = GM_getValue("cid_h");
-                x.push(this.id);
-                GM_setValue("cid_h", x);
-            } else {
-                this.className = "course_b"
-                document.querySelector("[cid='" + this.id + "']").style.display = '';
-                let x = GM_getValue("cid_h");
-                while (x.indexOf(this.id) != -1) {
-                    x.splice(x.indexOf(this.id), 1);
-                }
-                GM_setValue("cid_h", x);
-            }
-        })
-        course_b.innerHTML = course_ele[i].children[2].firstElementChild.innerHTML;
-        course_b_row.appendChild(course_b);
-    }
-    course_root.appendChild(course_b_row);
-    var course_s = creatdiv("course_s");
-    course_s.innerHTML = "显示课程菜单";
-    course_s.onclick = function () {
-        if (course_s.innerHTML == "显示课程菜单") {
-            course_s.innerHTML = "隐藏课程菜单";
-            course_root.style.top = "0px";
-        } else {
-            course_s.innerHTML = "显示课程菜单";
-            course_root.style.top = '-400px';
-        }
-    }
-    course_root.style.top = '-400px';
-    course_root.appendChild(course_s);
-    document.body.appendChild(course_root);
-    //载入隐藏课程
-    if (GM_getValue("cid_h") === undefined) {
-        let x = [];
-        GM_setValue("cid_h", x);
-    } else {
+  var course_ele = course_r.children[0].children;
+  var course_root = creatdiv("course_root");
+  var course_b_row = creatdiv("course_b_row");
+  p(course_ele)
+  for (let i = 0; i < course_ele.length; i++) {
+    let course_b = document.createElement("div");
+    course_b.className = ("course_b");
+    course_b.id = course_ele[i].getAttribute("cid");
+    course_b.addEventListener("click", function () {
+      let course_r = document.getElementById(this.id);
+      if (this.className == "course_b") {
+        this.className = "course_b_h"
+        document.querySelector("[cid='" + this.id + "']").style.display = 'none';
         let x = GM_getValue("cid_h");
-        for (let i = 0; i < x.length; i++) {
-            document.getElementById(x[i]).className="course_b_h"
-            document.querySelector("[cid='" + x[i] + "']").style.display = 'none';
+        x.push(this.id);
+        GM_setValue("cid_h", x);
+      } else {
+        this.className = "course_b"
+        document.querySelector("[cid='" + this.id + "']").style.display = '';
+        let x = GM_getValue("cid_h");
+        while (x.indexOf(this.id) != -1) {
+          x.splice(x.indexOf(this.id), 1);
         }
+        GM_setValue("cid_h", x);
+      }
+    })
+    course_b.innerHTML = course_ele[i].children[0].children[2].firstElementChild.innerHTML;
+    course_b_row.appendChild(course_b);
+  }
+  course_root.appendChild(course_b_row);
+  var course_s = creatdiv("course_s");
+  course_s.innerHTML = "显示课程菜单";
+  course_s.onclick = function () {
+    if (course_s.innerHTML == "显示课程菜单") {
+      course_s.innerHTML = "隐藏课程菜单";
+      course_root.style.top = "0px";
+    } else {
+      course_s.innerHTML = "显示课程菜单";
+      course_root.style.top = '-400px';
     }
-    //p("预定义输出结束");
+  }
+  course_root.style.top = '-400px';
+  course_root.appendChild(course_s);
+  document.body.appendChild(course_root);
+  //载入隐藏课程
+  if (GM_getValue("cid_h") === undefined) {
+    let x = [];
+    GM_setValue("cid_h", x);
+  } else {
+    let x = GM_getValue("cid_h");
+    for (let i = 0; i < x.length; i++) {
+      document.getElementById(x[i]).className = "course_b_h"
+      document.querySelector("[cid='" + x[i] + "']").style.display = 'none';
+    }
+  }
+  //p("预定义输出结束");
 })();
